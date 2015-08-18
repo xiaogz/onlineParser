f837b8bc55e99bd5932a40b52789391aa1069610
xiaogz <gz_xiao@live.com>
2015-08-18 16:06:42 -0400

    Fixed error stemming from redirect
    1. Added gem open_uri_redirections to Gemfile and the code
      * for now, only allow http to https redirections

--- parserService.rb
+++ parserService.rb
@@ -5,6 +5,7 @@ require 'nokogiri' # html smart parser tool
 require 'thin' # replaces WEbrick server
 require 'open-uri' # makes http GET requests into 'open'
 require 'json' # for obvious reasons
+require 'open_uri_redirections' # allows browser redirects (my setting only allows from http to https)
 
 get '/' do
   "Hi! To use our service, please send a http GET request from your app/software in the following format: '.../api/url/targetSite.com' and you will get the target site's page title and meta description"
@@ -13,8 +14,8 @@ end
 get '/api/url/:urlInput' do
   content_type :json
   customLink = params[:urlInput]
-  siteData = Nokogiri::HTML(open("https://" + customLink))
+  siteData = Nokogiri::HTML(open("http://" + customLink, :allow_redirections => :safe))
   pageTitle = siteData.title
   pageDescription = siteData.at_xpath("//meta[@name='description']/@content")
-  {:title => pageTitle, :description => pageDescription}.to_json
+  {:title => pageTitle, :description => pageDescription, :dateOfService => Time.now}.to_json
 end
